d4ae12b2a8512c4549b18ffdf9acf42fb9e121d5
adriElias <34091624+adriElias@users.noreply.github.com>
2026-03-01 18:31:41 +0100

    test: add JUnit 5 tests for salary calculations and invocations from main

--- src/main/java/org/workerOverride/App.java
+++ src/main/java/org/workerOverride/App.java
@@ -2,6 +2,21 @@ package org.workerOverride;
 
 public class App {
     public static void main(String[] args) {
+        Worker worker = new Worker("John", "Smith", 12.0);
+        OnsiteWorker onsiteWorker = new OnsiteWorker("Mary", "Johnson", 15.0);
+        OnlineWorker onlineWorker = new OnlineWorker("Peter", "Williams", 18.0);
 
+        int hours = 160;
+        System.out.println("--- WORKER ---");
+        System.out.println("Worker: " + worker);
+        System.out.println("Salary (" + hours + "hours): " + worker.calculateSalary(hours) + "€");
+
+        System.out.println("--- ONSITE WORKER ---");
+        System.out.println("Worker: " + onsiteWorker);
+        System.out.println("Salary (" + hours + "hours): " + onsiteWorker.calculateSalary(hours) + "€");
+
+        System.out.println("--- ONLINE WORKER ---");
+        System.out.println("Worker: " + onlineWorker);
+        System.out.println("Salary (" + hours + "hours): " + onlineWorker.calculateSalary(hours) + "€");
     }
 }
